c095b53701d51ce31c48ed855fc0d6fd2b52e30a
NicoJ <nicolasj@outlook.com>
2021-03-10 20:40:31 -0500

    can-sum additional fix for recursive lists

--- exercises/trees/can-sum.py
+++ exercises/trees/can-sum.py
@@ -24,7 +24,7 @@ def _can_sum(target, numarray, path, memo={}):
     return result
 
 # print(can_sum(0,[1])) # True
-# print(can_sum(1,[1])) # True //TODO shouldn't be an empty array
+# print(can_sum(1,[1])) # True 
 # print(can_sum(7,[3,4,8])) # True
 # print(can_sum(2,[3, 4])) # False
 # print(can_sum(3000, [17, 21, 7, 31, 23])) # True
@@ -55,7 +55,7 @@ def _any_sum_v2(target, numarray, memo={}):
     return result
 
 # print(any_sum_v2(0,[1])) # True
-# print(any_sum_v2(1,[1])) # True //TODO shouldn't be an empty array
+# print(any_sum_v2(1,[1])) # True 
 # print(any_sum_v2(7,[3,4,8])) # True
 # print(any_sum_v2(2,[3, 4])) # False
 # print(any_sum_v2(3000, [17, 21, 7, 31, 23])) # True
@@ -82,8 +82,9 @@ def _best_sum_v2(target, numarray, memo={}):
 
     for num in numarray:
         # this is probably the most important line of code from the day. 
-        # when modifying list of objects from python in a recursive call
-        # MAKE A FUKING DEEP COPY
+        # when modifying list of objects on a python recursive call
+        # MAKE A FUKING DEEP COPY as it will be always referencing the objects 
+        # and not the values
         reminder_combination = copy.deepcopy(_best_sum_v2(target - num, numarray, memo))
         if reminder_combination is not None: 
             reminder_combination.extend([num])
@@ -94,7 +95,7 @@ def _best_sum_v2(target, numarray, memo={}):
     return shortest
 
 print(best_sum_v2(0,[1])) # True
-print(best_sum_v2(1,[1])) # True //TODO shouldn't be an empty array
+print(best_sum_v2(1,[1])) # True 
 print(best_sum_v2(7,[3,4,8])) # True
 print(best_sum_v2(2,[3, 4])) # False
 print(best_sum_v2(8, [2, 3, 5]))
